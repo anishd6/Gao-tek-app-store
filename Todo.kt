6a0cfc07f7ff238f160a85c10f0ddec9898c692c
Ye Gong <60647568+BeliMove1996@users.noreply.github.com>
2021-11-22 07:48:33 +1100

    Delete Todo.kt

--- Todo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.todolist
-
-import android.icu.text.CaseMap
-
-data class Todo(
-    val title: String,
-    var isChecked: Boolean = false //false value is a default value
-) //very simple class without any logics but only constructors
